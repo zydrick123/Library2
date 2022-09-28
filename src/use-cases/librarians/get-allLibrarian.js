a151245377839bb3eba7eff8fb2f543bffd6f95f
Zydrick Czyrus Ygot <zydrickczyrus.ygot@biotechfarms.net>
2022-09-28 15:21:16 +0800

    with volume

--- src/use-cases/librarians/get-allLibrarian.js
+++ src/use-cases/librarians/get-allLibrarian.js
@@ -5,13 +5,14 @@ const getAllLibrarian = ({ librarianDB }) => {
         let librarianList = []
         const result = await librarianDB.getAllLibrarian({})  //fetch data from database
         for (let data of result.rows) {
+            let date = new Date(data.RegisterDate)
             const dataValue = {}
             dataValue.UserID = data.UserID
             dataValue.Firstname = data.Firstname
             dataValue.Middlename = data.Middlename
             dataValue.Lastname = data.Lastname
             dataValue.Gender = data.Gender
-            dataValue.RegisterDate = data.RegisterDate
+            dataValue.RegisterDate = date.getMonth() + '-' + date.getDay() + '-' + date.getFullYear()
 
 
             librarianList.push(dataValue)
