cc173cc4bb50118d9567a7c6deed8b1f927a69eb
Zydrick Czyrus Ygot <zydrickczyrus.ygot@biotechfarms.net>
2022-10-05 10:16:47 +0800

    fix login

--- src/routes/account/route.js
+++ src/routes/account/route.js
@@ -2,7 +2,7 @@ const { getAllAccountsController, addAccountController, updateAccountController,
 const passport = require('passport')
 const accountRoutes = ({ accountRouter, makeExpressCallback }) => {
     accountRouter.get('/api/accounts', passport.authenticate('jwt', { session: false }), makeExpressCallback(getAllAccountsController))
-        .post('/api/accounts/login', passport.authenticate('jwt', { session: false }), makeExpressCallback(loginAccountsController))
+        .post('/api/accounts/login', makeExpressCallback(loginAccountsController))
         .post('/api/accounts', passport.authenticate('jwt', { session: false }), makeExpressCallback(addAccountController))
         .patch('/api/accounts/:id', passport.authenticate('jwt', { session: false }), makeExpressCallback(updateAccountController))
     return accountRouter
