8849a6016d92ea02f5d784a99084d2c9b3c79746
Zydrick Czyrus Ygot <zydrickczyrus.ygot@biotechfarms.net>
2023-01-31 09:48:36 +0800

    fix find module

--- src/entities/issued_books/index.js
+++ src/entities/issued_books/index.js
@@ -1,5 +1,5 @@
-const registerIssued_Books_Entity = require('./register-issued_Book')
-const updateIssued_Books_Entity = require('./update-issued_Book')
+const registerIssued_Books_Entity = require('./register-issued_book')
+const updateIssued_Books_Entity = require('./update-issued_book')
 const services = Object.freeze({ registerIssued_Books_Entity, updateIssued_Books_Entity })
 
 module.exports = services, { registerIssued_Books_Entity, updateIssued_Books_Entity }
